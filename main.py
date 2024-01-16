82692a3cea0911265fe6b68163c3bfb32dff2548
ikari2k <pluta.m@gmail.com>
2024-01-16 19:01:41 +0100

    another method extraxted

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 user_prompt = "Type add or show or edit or complete or exit: "
 
-def get_todos():
-    with open('todos.txt', 'r') as file_local:
+def get_todos(filepath):
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local    
 
+def write_todos(filepath, todos_arg):
+    with open(filepath, 'w') as file_local:
+            file_local.writelines(todos_arg)
 
 while True:
     user_action = input(user_prompt)
@@ -13,14 +16,13 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:] 
         
-        todos = get_todos()
+        todos = get_todos('todos.txt')
         
         todos.append(todo + '\n')
         
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos('todos.txt', todos)
     elif user_action.startswith('show'):
-        todos = get_todos()
+        todos = get_todos('todos.txt')
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -31,13 +33,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = get_todos('todos.txt')
 
             new_todo = input('Enter new todo: ')
             todos[number] = new_todo + '\n'
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos('todos.txt', todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -45,16 +46,15 @@ while True:
         try:
             number = int(user_action[9:])
             
-            todos = get_todos()
+            todos = get_todos('todos.txt')
             
             index = number - 1 
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos('todos.txt', todos)
             
-            message = f"Todo \"{todo_to_remove}\" was removed fgrom the list."
+            message = f"Todo \"{todo_to_remove}\" was removed from the list."
             print(message)
         except IndexError:
             print('There is no item with that number.')    
