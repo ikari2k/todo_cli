dbf789b7e1d5ce6b0d678e843d0ec3cf03032282
ikari2k <pluta.m@gmail.com>
2024-01-17 13:49:48 +0100

    default params

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 user_prompt = "Type add or show or edit or complete or exit: "
 
-def get_todos(filepath):
+def get_todos(filepath='todos.txt'):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local    
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath='todos.txt'):
     with open(filepath, 'w') as file_local:
             file_local.writelines(todos_arg)
 
@@ -16,11 +16,11 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:] 
         
-        todos = get_todos('todos.txt')
+        todos = get_todos()
         
         todos.append(todo + '\n')
         
-        write_todos('todos.txt', todos)
+        write_todos(todos)
     elif user_action.startswith('show'):
         todos = get_todos('todos.txt')
 
@@ -33,12 +33,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
             new_todo = input('Enter new todo: ')
             todos[number] = new_todo + '\n'
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -46,13 +46,13 @@ while True:
         try:
             number = int(user_action[9:])
             
-            todos = get_todos('todos.txt')
+            todos = get_todos()
             
             index = number - 1 
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
             
             message = f"Todo \"{todo_to_remove}\" was removed from the list."
             print(message)
